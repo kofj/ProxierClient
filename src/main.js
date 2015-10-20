4f2faae2caab7350b21a737b5a7dd9daec381fc6
Frank Kung <kfanjian@gmail.com>
2015-10-20 23:13:09 +0800

    create main window without frame.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
 // main process for the app.
+
+'use strict';
+
+// require modules.
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+// declear variables.
+var mainWindow = null;
+
+/*
+listen event.
+*/
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800,
+        frame: false
+    });
+})
