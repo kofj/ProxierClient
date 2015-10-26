38272e254ee9a8eb776bc8d720327b49eefa651b
Frank Kung <kfanjian@gmail.com>
2015-10-27 00:00:23 +0800

    add is-login code block.

---
+++ src/modules/login.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var sysconfig = require('../../lib/sysconfig.js');
+var config = require('../../lib/configuration.js');
+var EventProxy = require('eventproxy');
+var request = require('request');
+
+var userInfo = config.get('user');
+var ep = new EventProxy();
+
+// check user is login.
+ep.tail('is-login', function() {
+    var options = {
+        url: sysconfig.api('islogin'),
+        headers: {
+            user: userInfo.name,
+            token: userInfo.token,
+        }
+    }
+
+    // send request
+    request.get(options, function(err, response, body) {});
+});
